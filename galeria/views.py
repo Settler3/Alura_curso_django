e3c437d9eddbf5f9d9d3fec3f8c2776bb3529f25
Entalpita <mateus9878@gmail.com>
2023-07-19 04:17:31 -0300

    Adicionando models e dinamizando pagina .html

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'alura_space-projeto_front/index.html')
+    return render(request, 'alura_space-projeto_front/index.html', {"cards": Fotografia.objects.all()})
 
-def imagem(request):
-    return render(request, 'alura_space-projeto_front/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'alura_space-projeto_front/imagem.html', {"foto": fotografia})
