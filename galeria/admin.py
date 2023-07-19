8b75af0550ec157c5216255aef28932634514347
Entalpita <mateus9878@gmail.com>
2023-07-19 06:35:54 -0300

    Atualizando requirements e mexendo nas migrations

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from galeria.models import Fotografia, Categoria
 
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "categoria", "data_publicacao", "publicada")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+class ListandoCategorias(admin.ModelAdmin):
+    list_display = ("id", "nome", "descricao")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_per_page = 10
 # Register your models here.
+admin.site.register(Fotografia, ListandoFotografias)
+admin.site.register(Categoria, ListandoCategorias)
--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'alura_space-projeto_front/index.html', {"cards": Fotografia.objects.all()})
+    return render(request, 'alura_space-projeto_front/index.html', {"cards": Fotografia.objects.order_by("-data_publicacao").filter(publicada=True)})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
