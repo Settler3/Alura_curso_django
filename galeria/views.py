d04cdcb5fc52077da655330f655883631615aff4
Entalpita <mateus9878@gmail.com>
2023-07-29 13:53:26 -0300

    finalizando aula 3

--- galeria/views.py
+++ galeria/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'alura_space-projeto_front/index.html', {"cards": Fotografia.objects.order_by("-data_publicacao").filter(publicada=True)})
+    if not request.user.is_authenticated:
+        messages.error(request, 'Efetue login')
+        return redirect('login')
+    else:
+        return render(request, 'galeria/index.html', {"cards": Fotografia.objects.order_by("-data_publicacao").filter(publicada=True)})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'alura_space-projeto_front/imagem.html', {"foto": fotografia})
+    return render(request, 'galeria/imagem.html', {"foto": fotografia})
 
+@login_required
 def buscar(request):
     fotografias = Fotografia.objects.order_by("-data_publicacao").filter(publicada=True)
 
@@ -15,4 +22,4 @@ def buscar(request):
         nome_busca = request.GET['buscar']
         if nome_busca:
             fotografias = fotografias.filter(nome__icontains=nome_busca)
-    return render(request, 'alura_space-projeto_front/buscar.html', {"cards": fotografias})
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
